8e93f2d4c8a1272c5ff1575cfa02904b3a877177
djneils <southinn@colchsfc.ac.uk>
2024-02-08 13:58:46 +0000

    Update northwind.sql

--- northwind.sql
+++ northwind.sql
@@ -6,8 +6,8 @@ CREATE TABLE bank_accounts (
 );
 
 INSERT INTO bank_accounts (surname, forename, balance) VALUES
-('Smith', 'John', 1000.00),
-('Doe', 'Jane', 1500.50),
-('Brown', 'Chris', 2000.00),
-('Wilson', 'Pat', 2500.75),
-('Taylor', 'Alex', 3000.00);
+('Smith', 'John', 1000),
+('Doe', 'Jane', 1500),
+('Brown', 'Chris', 2000),
+('Wilson', 'Pat', 2500),
+('Taylor', 'Alex', 3000);
